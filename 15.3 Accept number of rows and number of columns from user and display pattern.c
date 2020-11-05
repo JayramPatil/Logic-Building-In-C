4cc183abdf8c9cdc1f3fa11d9b8a4deed6891f0a
Jayram <Jayram1234patil@gmail.com>
2020-11-05 08:25:21 +0530

    Assignment-15

---
+++ 15.3 Accept number of rows and number of columns from user and display pattern.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int iNo1=1,iNo2=1;
+
+    for(iNo1=1;iNo1<=iRow;iNo1++)
+    {
+        for(iNo2=iCol;iNo2>0;iNo2--)
+        {
+            printf(" %d ",iNo2);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter The Number Of Rows And Columns => ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
+
---
+++ 15.5 Accept number of rows and number of columns from user and display pattern.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int iNo1=1,iNo2=1;
+
+    for(iNo1=1;iNo1<=iRow;iNo1++)
+    {
+        for(iNo2=1;iNo2<=iCol;iNo2++)
+        {
+            printf(" %d ",iNo1);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter The Number Of Rows And Columns => ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
+
+
