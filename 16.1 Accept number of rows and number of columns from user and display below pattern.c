bdefd0b23a2d682e2a853b7a555ca2b1e0856778
Jayram <Jayram1234patil@gmail.com>
2020-11-06 08:20:15 +0530

    Assignment-16

---
+++ 16.1 Accept number of rows and number of columns from user and display below pattern.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int iNo1=1,iNo2=1;
+    char Ch='A';
+    for(iNo1=1;iNo1<=iRow;iNo1++)
+    {
+        for(iNo2=1,Ch='A';iNo2<=iCol;iNo2++,Ch++)
+        {
+            printf(" %c ",Ch);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter The Number Of Rows And Columns => ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
+
---
+++ 16.2 Accept number of rows and number of columns from user and display below pattern.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int iNo1=1,iNo2=1;
+    int Ch=0;
+    for(iNo1=1;iNo1<=iRow;iNo1++)
+    {
+        for(iNo2=1;iNo2<=iCol;iNo2++)
+        {
+            if(iNo1%2!=0)
+            {
+                Ch=64;
+                printf(" %c ",Ch+iNo2);
+            }
+            else
+            {
+                Ch=96;
+                printf(" %c ",Ch+iNo2);
+            }
+
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter The Number Of Rows And Columns => ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
+
+
