502b372882082f71783984bb0114a906e90302cf
Jayram <Jayram1234patil@gmail.com>
2021-01-23 00:06:32 +0530

    Assignment-47

---
+++ LB - 47.5/LB - 47.5/47.5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void DisplayNum(int iNo)			// Using ASCII
+{
+	if (iNo == 0)
+	{
+		return;
+	}
+	DisplayNum(iNo - 1);
+	printf(" %c ", 96 + iNo);
+}
+int main()
+{
+	int iNo = 0;
+
+	printf("Enter The Number => ");
+	scanf_s("%d", &iNo);
+
+	DisplayNum(iNo);
+
+	return 0;
+}
